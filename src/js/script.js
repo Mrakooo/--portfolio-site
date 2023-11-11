97f1bf53dd3d97627043b34d648259868ef14600
Mrakooo <mrandoksss1@gmail.com>
2023-11-11 19:42:54 +0200

    -

--- src/js/script.js
+++ src/js/script.js
@@ -50,74 +50,30 @@ document.getElementById("goToPage_2").addEventListener("click", function() {
   window.location.href = "index2.html";
 });
 
+
+
+
+document.querySelector('a[href="#section_contacts"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.getElementById('section_contacts').scrollIntoView({ behavior: 'smooth' });
+});
 /*
-// Отримуємо всі кнопки
-const buttons = document.querySelectorAll('.button_text');
+document.querySelector('a[href="#section_contacts_2"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.getElementById('section_contacts_2').scrollIntoView({ behavior: 'smooth' });
+});*/
 
-// Отримуємо всі контент-секції
-const contentSections = document.querySelectorAll('.content_section');
-
-// Додаємо обробник події для кожної кнопки
-buttons.forEach(button => {
-button.addEventListener('click', () => {
-  // Знайдіть ID контенту, який відповідає цій кнопці
-  const targetContentId = button.getAttribute('data-target');
-
-  // Знайдіть контент-секцію, яка відповідає цьому ID
-  const targetContent = document.getElementById(targetContentId);
-
-  // Видаляємо клас "active" з усіх кнопок
-  buttons.forEach(btn => btn.classList.remove('active'));
-
-  // Додаємо клас "active" до натиснутої кнопки
-  button.classList.add('active');
-
-  // Приховуємо всі контент-секції
-  contentSections.forEach(section => section.style.display = 'none');
-
-  // Показуємо контент, відповідний обраній кнопці
-  targetContent.style.display = 'block';
-});
+document.querySelector('a[href="#section_about_me"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.getElementById('section_about_me').scrollIntoView({ behavior: 'smooth' });
 });
 
-// За замовчуванням показуємо вміст для кнопки "About me"
-document.getElementById('about_me_Content').style.display = 'block';
-document.getElementById('about_me_Button').classList.add('active');
+document.querySelectorAll('a[href^="index.html#section_about_me"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-*/
-/*
-document.getElementById("about_me_Button").addEventListener("click", function() {
-  setActiveButton(this);
-  displayContent("about_me_Content");
+    document.querySelector(this.getAttribute('index.html#section_about_me')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 });
-
-document.getElementById("skills_Button").addEventListener("click", function() {
-  setActiveButton(this);
-  displayContent("skills_Content");
-});
-
-document.getElementById("education_Button").addEventListener("click", function() {
-  setActiveButton(this);
-  displayContent("education_Content");
-});
-
-function setActiveButton(button) {
-  var buttons = document.querySelectorAll(".button_text");
-  for (var i = 0; i < buttons.length; i++) {
-	 buttons[i].classList.remove("active_button");
-  }
-  button.classList.add("active_button");
-}
-
-function displayContent(contentId) {
-  var contents = document.querySelectorAll(".content_section");
-  for (var i = 0; i < contents.length; i++) {
-	 contents[i].style.display = "none";
-  }
-  document.getElementById(contentId).style.display = "block";
-}
-
-// За замовчуванням показуємо вміст для кнопки "About me"
-document.getElementById("about_me_Content").style.display = "block";
-
-*/
